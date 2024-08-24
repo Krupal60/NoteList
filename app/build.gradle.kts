c6e83658b5db0787249f3ef283f4c7ede55a9e0e
Krupal Vekariya <vkrupal241@gmail.com>
2024-08-24 16:28:14 +0530

    app version to 1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.note.list"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.4"
+        versionCode = 5
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
